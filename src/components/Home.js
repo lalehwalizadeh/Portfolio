0620e55abe833575452d64f9b401b229489d0f3b
LalehWalizadeh <lalah.walizadah2022@gmail.com>
2023-11-18 12:49:22 -0800

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,57 +1,40 @@
+import { motion } from 'framer-motion';
 import Capture from '../Images/Capture.jpg';
 import './Home.css';
 import './botton.css';
 import React from 'react';
 
 export default function Home() {
-  return (
-    
-      <div className='design'>
-
-        <div className="side">
-          <div className="pic "><img src={Capture} alt="my_picture" className='img' /></div>
-          <div className="intro">
-            <h1>Ms.Laleh Walizadeh </h1>
-            <h6>Frontend Web Developer</h6>
-            <br />
-            <span>
-
-              <a href='/about'className='btn_design '> More About Me</a>
-              <a href='/works'className='btn_design '> Portfolio </a>
-
-            </span>
-          </div>
-        </div>
-
-      </div>
-   
-
-  );
+	return (
+		<div className='design'>
+			<motion.div
+				className='side'
+				variants={{
+					hidden: { opacity: 0, y: 100 },
+					visible: { opacity: 1, y: 5 },
+				}}
+				initial='hidden'
+				animate='visible'
+				transition={{ duration: 1, delay: 0.25 }}>
+				<div className='pic '>
+					<img src={Capture} alt='my_picture' className='img' />
+				</div>
+				<div className='intro'>
+					<h1>Ms.Laleh Walizadeh </h1>
+					<h6>Frontend Web Developer</h6>
+					<br />
+					<span>
+						<a href='/about' className='btn_design '>
+							{' '}
+							More About Me
+						</a>
+						<a href='/works' className='btn_design '>
+							{' '}
+							Portfolio{' '}
+						</a>
+					</span>
+				</div>
+			</motion.div>
+		</div>
+	);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
